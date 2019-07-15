678bf5fdfab72d425a32c8af3615e24ad0f0c334
Nadia <kim.nadia28@gmail.com>
2019-07-15 16:14:57 -0700

    final changes, modified code

--- script.js
+++ script.js
@@ -10,43 +10,52 @@ const winningRows = [
     [2,4,6]
 ]
 
+//helper functions
+
 // array of squares of the board
 const board = () => Array.from(document.getElementsByClassName('square'));
 
 //identifying each square's id, excluding sq
 const squareIdToNum = (squareEl) => Number.parseInt(squareEl.id.replace('sq', ''));
 
-
 //identifying empty spaces for the participants
-const emptySpace = () => board().filter(_squareEl => _squareEl.innerText === '');
+const emptySpace = () => board().filter(squareEl => squareEl.innerText === '');
 
 //check if all 3 spaces have the same inner text x's or o's
-const sameInnerText = (arr) => arr.every(_squareEl => _squareEl.innerText === arr[0].innerText && _squareEl.innerText !== '');
+const sameInnerText = (arr) => arr.every(squareEl => squareEl.innerText === arr[0].innerText && squareEl.innerText !== '');
 
 // taking turns 
 const takeTurns = (index, letter) => board()[index].innerText = letter;
 // choosing random available empty space 
 const nextPlayerOpt = () => squareIdToNum(emptySpace()[Math.floor(Math.random() * emptySpace().length)]);
 
+
+
+
+
+
 // Finish the game
-const endGame = () => { 
-    console.log('game over'); 
+const endGame = (winningSequence) => { 
+    winningSequence.forEach(squareEl => squareEl.classList.add('winner'));
     disableListener(); 
 }
 
+
+
+// checking if reached winning sequence
 const checkWin = () => {
     let victory = false;
-     winningRows.forEach(_r => {
+    winningRows.forEach( r => {
         const _board = board();
-        const sequence = [_board[_r[0]], _board[_r[1]], _board[_r[2]]];
-        console.log(sequence)
-            if(sameInnerText(sequence)) {
-                victory = true;
-                endGame(sequence);
-            }
+        const sequence = [_board[r[0]], _board[r[1]], _board[r[2]]];
+        if(sameInnerText(sequence)) {
+            victory = true;
+            endGame(sequence);           
+        }
     });
-    return victory;
+    return victory;   
 };
+
 
 
 //other player's turn 
@@ -54,8 +63,7 @@ const nextPlayerTurn = () => {
     disableListener();
     setTimeout(() => {
         takeTurns(nextPlayerOpt(), 'O');
-        if(!checkWin());
-        enableListener();
+        if(!checkWin()) enableListener();
     }, 1000);
 }
 
@@ -63,15 +71,14 @@ const nextPlayerTurn = () => {
 //click function
 const clickFunc = (event) => {
     takeTurns(squareIdToNum(event.target), 'X');
-    if(!checkWin());
-    nextPlayerTurn();
+    if(!checkWin())  nextPlayerTurn();
 };
 
 // add event listener click
-const enableListener = () => board().forEach(_squareEl => _squareEl.addEventListener('click', clickFunc));
+const enableListener = () => board().forEach(squareEl => squareEl.addEventListener('click', clickFunc));
 
 // remove event listener
-const disableListener = () => board().forEach(_squareEl => _squareEl.removeEventListener('click', clickFunc));
+const disableListener = () => board().forEach(squareEl => squareEl.removeEventListener('click', clickFunc));
 
 enableListener();
 
